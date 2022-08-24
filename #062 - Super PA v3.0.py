c463c756cfb49863594da1ec2e52f62cdacf8763
felixoakz <feliz@oakz.org>
2022-08-24 09:49:38 -0300

    Starting folder

--- #062 - Super PA v3.0.py
+++ #062 - Super PA v3.0.py
@@ -2,18 +2,14 @@ termo = int(input('Primeiro termo da PA: '))
 razao = int(input('Razao da PA: '))
 tamPA = 10
 ultimotermo = termo + (razao*tamPA)
-
 while termo != ultimotermo:
     print(f'{termo}', end=' | ')
     termo += razao
-
-    if termo == ultimotermo:
-        tamPA = int(input('\nQuantos termos quer a mais?: '))
-        ultimotermo = termo + (razao * tamPA)
-
-        while termo != ultimotermo:
-            print(f'{termo}', end=' | ')
-            termo += razao
-
+    while tamPA != 0:
+        if termo == ultimotermo:
+            tamPA = int(input('\nQuantos termos quer a mais?: '))
+            ultimotermo = termo + (razao * tamPA)
+            while termo != ultimotermo:
+                print(f'{termo}', end=' | ')
+                termo += razao
 print('\nPrograma encerrado.')
-
