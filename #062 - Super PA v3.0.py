090818dcfd238a9943390693ee79c735b9a5b6b2
felixoakz <feliz@oakz.org>
2022-08-25 10:10:05 -0300

    Merge remote-tracking branch 'origin/master'

--- #062 - Super PA v3.0.py
+++ #062 - Super PA v3.0.py
@@ -1,15 +1,21 @@
-termo = int(input('Primeiro termo da PA: '))
-razao = int(input('Razao da PA: '))
+t = int(input('Primeiro termo da PA: '))
+r = int(input('Razao da PA: '))
 tamPA = 10
-ultimotermo = termo + (razao*tamPA)
-while termo != ultimotermo:
-    print(f'{termo}', end=' | ')
-    termo += razao
+ultimoT = t + (r * tamPA)
+
+while t != ultimoT:
+    print(f'{t}', end=' | ')
+    t += r
+
     while tamPA != 0:
-        if termo == ultimotermo:
+        if t == ultimoT:
             tamPA = int(input('\nQuantos termos quer a mais?: '))
-            ultimotermo = termo + (razao * tamPA)
-            while termo != ultimotermo:
-                print(f'{termo}', end=' | ')
-                termo += razao
+            ultimoT = t + (r * tamPA)
+
+            while t != ultimoT:
+                print(f'{t}', end=' | ')
+                t += r
 print('\nPrograma encerrado.')
+
+
+#ao finalizar testado e funcionando. algo quebrou e corrija depois!
---
+++ #063 - Sequência de Fibonacci v1.0.py
@@ -0,0 +1,12 @@
+termos = int(input('Quantos termos voce quer mostrar?: '))
+n = 0
+nn = 1
+cont = 2
+print(f'{n} - {nn}', end=' - ')
+while cont != termos:
+    nnn = n + nn
+    print(f'{nnn}', end=" - ")
+    n = nn
+    nn = nnn
+    cont += 1
+print('Fim')
