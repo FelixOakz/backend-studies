a50f607da30bb6f06d80d1d418184e5092b8d3c0
felixoakz <feliz@oakz.org>
2022-08-25 20:29:21 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ Draft.py
@@ -0,0 +1,4 @@
+i = 0
+for x in range(100):
+    print(i)
+    i += 1
--- Multithreadingexample.py
+++ Multithreadingexample.py
@@ -1,18 +1,27 @@
 from threading import *
+import threading
 from time import sleep
 
+lock = threading.Lock()
+
 class Example(Thread):
-    def run(self):
-        for i in range(5):
-            print("Hello from Example")
-            sleep(1)
+   def run(self):
+       for i in range(5):
+           lock.acquire()
+           print('Lock 1 acquired')
+           print("Hello from Example")
+           sleep(1)
+           lock.release()
 
 
 class ExampleTwo(Thread):
-    def run(self):
-        for i in range(5):
-            print("Hello from ExampleTwo")
-            sleep(1)
+   def run(self):
+       for i in range(5):
+           lock.acquire()
+           print("Hello from ExampleTwo")
+           print('Lock 2 acquired')
+           sleep(1)
+           lock.release()
 
 
 example = Example()
