56111514cbc1e8065256c9119a2059c563efd598
felixoakz <feliz@oakz.org>
2022-08-26 09:43:20 -0300

    Merge remote-tracking branch 'origin/master'

--- #064 - Tratando vários valores v1.0.py
+++ #064 - Tratando vários valores v1.0.py
@@ -2,5 +2,5 @@ tot = cont = num = 0
 while num != 999:
     tot += num
     cont += 1
-    num = int(input('Digite um numero (999 para parar): '))
+    num = int(input('Digite um numero [999 para parar]: '))
 print(f'Foram digitados {cont-1} e a soma entre eles foi {tot}')
