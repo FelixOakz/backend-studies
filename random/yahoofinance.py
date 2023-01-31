9460200c4ab98f8fb5a6fc8fe359031598a05807
felixoakz <felix@oakz.org>
2023-01-30 21:09:02 -0300

    delete this shit

--- random/yahoofinance.py
+++ random/yahoofinance.py
@@ -5,15 +5,15 @@ def lookup(symbol):
     """Look up quote for symbol."""
     symbol.upper().strip()
 
-    if symbol[-1].isdigit():
-        symbol = symbol + '.SA'
-    try:
-        ticker = yf.Ticker(symbol)
-        todayData = ticker.history(period='1d')
-        return round(todayData['Close'][0], 2)
-    except Exception as e:
-        return f"Error: Could not retrieve stock data for symbol '{symbol}' - {str(e)}"
+    ticker = yf.Ticker(symbol)
+    todayData = ticker.history(period='1d')
+    info = yf.ticker(symbol).info
+    price = round(todayData['Close'][0], 2)
+    return {
+        "name": info['longName'],
+        "price": float(price),
+        "symbol": symbol
+    }
 
-
-stck = input('enter stock: ')
-print(lookup(stck))
+stock = input('enter stock: ')
+print(lookup(stock))
