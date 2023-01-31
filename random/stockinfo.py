bfc7a6aa6fd6628ad68fbc2c48e2cbb961df8afb
felixoakz <felix@oakz.org>
2023-01-31 18:37:48 -0300

    function with alphavintage api

--- random/stockinfo.py
+++ random/stockinfo.py
@@ -1,4 +1,12 @@
-from googlefinance import getQuotes
-import json
 
-print json.dumps(getQuotes('tsla'), indent=2)
+import requests
+
+def get_stock_data(symbol):
+    api_key = "PFDIMYSMNA5J1BUE"
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={api_key}"
+    response = requests.get(url)
+    stock_data = response.json()
+    company_name = stock_data["Global Quote"]["01. symbol"]
+    closing_price = float(stock_data["Global Quote"]["05. price"])
+
+    return company_name, closing_price
