c2c92725c2e1d24288626cd9156c8d45b2b0dcb9
felixoakz <felix@oakz.org>
2023-02-01 10:04:58 -0300

    stock function completed

--- random/stockinfo.py
+++ random/stockinfo.py
@@ -1,33 +1,50 @@
 import requests
 import csv
 
+
 def load_nasdaq_symbols():
+    """Load list of nasdaq companies"""
     symbols = {}
-    with open('webstudies/trainingTrader/nasdaq_companies.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader) # skip the header row
-        for row in reader:
-            symbol, name = row
-            symbols[symbol] = name
+    try:
+        with open('webstudies/trainingTrader/nasdaq_companies.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)  # skip the header row
+            for row in reader:
+                symbol, name = row
+                symbols[symbol] = name
+
+    except Exception as e:
+        print("An error occurred while reading the CSV file", e)
+        return {}
     return symbols
 
 
-def get_company_name(symbol):
-    return nasdaq_symbols.get(symbol, "Unknown Company")
+def lookup(symbol):
+    """Look up quote for symbol."""
 
-def get_stock_data(symbol):
+# contact api
+    symbol = symbol.upper().strip()
     api_key = "PFDIMYSMNA5J1BUE"
     url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={api_key}"
-    response = requests.get(url)
-    stock_data = response.json()
-    company_name = stock_data["Global Quote"]["01. symbol"]
-    closing_price = float(stock_data["Global Quote"]["05. price"])
+    try:
+        response = requests.get(url)
 
-    return company_name, closing_price, stock_data
+        # parse response
+        stock_data = response.json()
+        company_symbol = stock_data["Global Quote"]["01. symbol"]
+        closing_price = float(stock_data["Global Quote"]["05. price"])
 
-nasdaq_symbols = load_nasdaq_symbols()
+    except (requests.RequestException, KeyError, TypeError, ValueError):
+        return None
 
-company_symbol = input("Company Symbol: ").upper().strip()
+    nasdaq_symbols = load_nasdaq_symbols()
+    company_name = nasdaq_symbols.get(symbol, "Unknown Company")
 
-company_name, closing_price, stock_data = get_stock_data(company_symbol)
-print(company_symbol, get_company_name(company_symbol), closing_price)
+    return {
+        "name": company_name,
+        "price": closing_price,
+        "symbol": company_symbol
+    }
+
+
+print(lookup('tsla'))
