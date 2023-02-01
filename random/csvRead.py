7b346aa316fd95f814e47edded5a4bbc75ef7686
felixoakz <felix@oakz.org>
2023-01-31 21:08:47 -0300

    stock function updated

--- random/csvRead.py
+++ random/csvRead.py
@@ -1,6 +1,6 @@
 import csv
 # with is a keyword that dismisses need for closing file
-with open("csvtest.csv") as file:
+with open("backendstudies/random/csvtest.csv") as file:
     contents = csv.reader(file)
     next(contents)
     for row in contents:
--- random/stockinfo.py
+++ random/stockinfo.py
@@ -1,5 +1,19 @@
-
 import requests
+import csv
+
+def load_nasdaq_symbols():
+    symbols = {}
+    with open('webstudies/trainingTrader/nasdaq_companies.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader) # skip the header row
+        for row in reader:
+            symbol, name = row
+            symbols[symbol] = name
+    return symbols
+
+
+def get_company_name(symbol):
+    return nasdaq_symbols.get(symbol, "Unknown Company")
 
 def get_stock_data(symbol):
     api_key = "PFDIMYSMNA5J1BUE"
@@ -9,4 +23,11 @@ def get_stock_data(symbol):
     company_name = stock_data["Global Quote"]["01. symbol"]
     closing_price = float(stock_data["Global Quote"]["05. price"])
 
-    return company_name, closing_price
+    return company_name, closing_price, stock_data
+
+nasdaq_symbols = load_nasdaq_symbols()
+
+company_symbol = input("Company Symbol: ").upper().strip()
+
+company_name, closing_price, stock_data = get_stock_data(company_symbol)
+print(company_symbol, get_company_name(company_symbol), closing_price)
