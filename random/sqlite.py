9d5960ce8a69c48898d1385093a30cdfd39fabcf
felixoakz <felix@oakz.org>
2023-02-02 18:54:45 -0300

    sqlite more functionalities

--- random/sqlite.py
+++ random/sqlite.py
@@ -4,4 +4,19 @@ conn = sqlite3.connect('test.db')
 
 cursor = conn.cursor()
 
-cursor.execute('')
+cursor.execute("""
+               CREATE TABLE testing (
+                   first_name text,
+                   last_name text,
+                   email text
+               )
+               """)
+
+conn.commit()
+
+conn.close()
+
+conn.execute("SELECT * from test")
+conn.fetchone()
+conn.fetchmany(3)
+conn.fetchall()
