4b7092cb784704466c7e63a2f1bb4d3eb58efe7e
felixoakz <felix@oakz.org>
2023-02-13 16:49:13 -0300

    sqlitexpress

--- random/sqlitexpress.py
+++ random/sqlitexpress.py
@@ -1,11 +1,14 @@
+import sqlite3
+
+
 def execute_query(query, params=None, fetch=True):
-	"""
-	-> a function to execute sqlite3 queries
-	:param query: sql querie with unnamed parameters (?) in place of variables
-	:param params: parameters in order and inside a TUPLE
+    """
+    -> a function to execute sqlite3 queries
+    :param query: sql querie with unnamed parameters (?) in place of variables
+    :param params: parameters in order and inside a TUPLE
     :param fetch: set to True if querie awaits some return value. False otherwise.
-	:return: list of dictionary with items.
-	"""
+    :return: list of dictionary with items.
+    """
     with sqlite3.connect("finance.db") as conn:
         cursor = conn.cursor()
 
