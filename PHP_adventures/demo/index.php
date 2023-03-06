93ee8c776a36ff9bbf913f36e5f7aaae9a7699be
felixoakz <felix@oakz.org>
2023-03-05 21:32:24 -0300

    php study

--- PHP_adventures/demo/index.php
+++ PHP_adventures/demo/index.php
@@ -6,12 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+    <style>
+    body {
+        scroll-behavior: auto;
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+    }
+    </style>
 </head>
 
 <body>
     <h1>
         <?php
-            echo "Hello, FDP";
+        echo 'You have read "Dark Matter."'
         ?>
     </h1>
 </body>
