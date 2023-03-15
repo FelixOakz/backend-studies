0eaafc8826454aed8a318347cffe4c6f9da6951f
felixoakz <felix@oakz.org>
2023-03-15 10:59:26 -0300

    php beginner

--- PHP_adventures/index.php
+++ PHP_adventures/index.php
@@ -15,29 +15,3 @@ printNum(30);
 function printNum($n){
     echo $n;
 }
-
-
-class Pessoa {
-
-    public $nome;
-    public $idade;
-
-    public function __construct($nome, $idade) {
-    $this->nome = $nome;
-    $this->idade = $idade;
-    }
-
-    public function printNomeEIdade(){
-        echo $this->nome;
-        echo '<br>';
-        echo $this->idade;
-    }
-}
-
-// intanciar a classe , precisa ser chamada assim como uma funcão
-
-$pessoa = new Pessoa('Guilherme', '26');
-
-$arr = ['guilherme', 'joao', 'felipe'];
-
-echo $arr[1];
