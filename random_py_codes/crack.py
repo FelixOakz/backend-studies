d1c3eaa84132f7b21400232d61fda8c9b081821a
felixoakz <felix@oakz.org>
2023-08-27 15:54:10 -0300

    crack

--- random_py_codes/crack.py
+++ random_py_codes/crack.py
@@ -1,4 +1,3 @@
-
 '''from string import digits
 # giving access to all ascii characters, decimal digits and punctuation marks
 from itertools import product
@@ -12,7 +11,7 @@ for passcode in product(digits, repeat=4):
 	print(*passcode)
 '''
 import itertools
-
+import time
 
 '''
 map() converts our input into integers to match the type of the contents of 'a' 
@@ -21,12 +20,17 @@ and tuple() makes it a tuple to match the type of combinations.
 
 password = tuple(map(int, input('Enter 4 digit password: ')))
 
-digits = list(range(0, 9))
+digits = list(range(0, 10))
+start_time = time.time()
 
-for passcode in itertools.product(digits, repeat=4):
-    # print(passcode)
+for passcode in itertools.product(digits, repeat=6):
+    print(f"-> {passcode} ")
     if passcode == password:
-        print("-----> The password is: " + ''.join(str(i) for i in password))
+        print("\n\n-----> The password is: " + ''.join(str(i) for i in password))
         break
-    
-print("Programa finalizado em {} segundos.")
+
+execution_time = time.time() - start_time
+
+formatted_seconds = "{:.5f}".format(execution_time)
+
+print(f'\nThe execution time in seconds is: {formatted_seconds} seconds')
